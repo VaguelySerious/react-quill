9e803197621e1767c0dc75345c9a20f11bae79a6
RobertStanyon <stanyon.robert@gmail.com>
2025-01-21 10:57:50 +0000

    fix editor crash

--- src/index.tsx
+++ src/index.tsx
@@ -289,6 +289,7 @@ class ReactQuill extends React.Component<ReactQuillProps, ReactQuillState> {
   destroyEditor(): void {
     if (!this.editor) return;
     this.unhookEditor(this.editor);
+    delete this.editor;
   }
 
   /*
